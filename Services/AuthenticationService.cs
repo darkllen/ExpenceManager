2f01fd38cf857a6850031e57b831357951492b7e
darkllen <yankiniigor@gmail.com>
2021-04-11 01:23:05 +0300

    add encryption for user pass

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -19,9 +19,20 @@ namespace Services
             if (String.IsNullOrWhiteSpace(authUser.Login) || String.IsNullOrWhiteSpace(authUser.Password))
                 throw new ArgumentException("Login or Password is Empty");
             var users = await _storage.GetAllAsync();
-            var dbUser = users.FirstOrDefault(user => user.Login == authUser.Login && user.Password == authUser.Password);
-            if (dbUser == null)
+            UserDB dbUser = null;
+            try
+            {
+                dbUser = users.FirstOrDefault(user =>
+                    user.Login == authUser.Login &&
+                    Encryption.Decrypt(user.Password, authUser.Password) == authUser.Password);
+                if (dbUser == null)
+                    throw new Exception();
+            }
+            catch (Exception)
+            {
                 throw new UserException("Wrong Login or Password");
+            }
+
 
             CurrentUser = new User(dbUser.Name, dbUser.Surname, dbUser.Email, dbUser.Guid);
             return CurrentUser;
@@ -39,7 +50,7 @@ namespace Services
                 String.IsNullOrWhiteSpace(regUser.Password) || 
                 String.IsNullOrWhiteSpace(regUser.Email))
                 throw new ArgumentException("Login, Password, Name, Surname or Email is Empty");
-            dbUser = new UserDB(regUser.Login, regUser.Password, regUser.Name, regUser.Surname, regUser.Email);
+            dbUser = new UserDB(regUser.Login, Encryption.Encrypt(regUser.Password, regUser.Password), regUser.Name, regUser.Surname, regUser.Email);
             await _storage.AddOrUpdateAsync(dbUser);
             return true;
         }
